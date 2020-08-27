eda9062b391da8f8d024314cbebca34795e6987d
Tania Sainvilus <taniasainvilus@Tanias-Air.hsd1.ga.comcast.net>
2020-08-27 14:34:44 -0400

    edit registration

--- src/components/Authorization/Register.jsx
+++ src/components/Authorization/Register.jsx
@@ -42,23 +42,23 @@ export default function Register(props) {
         <ErrorMsg message={error} clearError={() => setError(undefined)} />
       )}
             <form className="form-auth" onSubmit={submit}>
+            <label htmlFor="register-email">Email</label>
             <input 
             id="register-email" 
-            placeholder="Email"
             type="email" 
             onChange={(event) => setEmail(event.target.value)}
             />
 
+            <label htmlFor="register-username">Username</label>
             <input 
             id="register-username" 
-            placeholder="Username"
             type="text"
             onChange={(event) => setUsername(event.target.value)}
             />
 
+            <label htmlFor="register-password">Password</label>
             <input 
             id="register-password" 
-            placeholder="Password"
             type="password"
             onChange={(event) => setPassword(event.target.value)}
             />
